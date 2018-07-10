d8f655085e041f395ff7f78e2b165f2ee4962dde
Al Smit <solanus@gmail.com>
2018-07-10 10:49:40 +0100

    mult players functionality added to game

--- game.py
+++ game.py
@@ -4,7 +4,10 @@ week_1 = [
      'result': 1},
     {'game': 2,
      'teams':['Spurs', 'Leicester'],
-     'result': 2}]
+     'result': 1},
+    {'game': 3,
+     'teams':['Swansea', 'Stoke'],
+     'result': 3}]
 
 class Question:
     def __init__(self, home, away, correct):
@@ -16,41 +19,55 @@ class Question:
         print(f'{self.home} vs {self.away}')
 
 
-
 class Player:
     def __init__(self, name='', score=0, answer=0):
         self.name = name
         self.score = score
 
-
     def prediction(self):
-        answer = input('Enter 1 for Home Win, 2 for a Draw and 3 for an Away Win')
+        answer = input('Enter your prediction.. ')
         self.answer = int(answer)
 
+def ansConv(guess):
+    if guess == 1:
+        ansStr = 'Home Win'
+    elif guess == 2:
+        ansStr = 'Draw'
+    else:
+        ansStr = 'Away Win'
+
+    return ansStr
 
 
 def main():
+    players = []
+    num_players = input('Please enter number of players..')
+    for player in range(int(num_players)):
+        #print(f'Player {player +1} created')
+        player = Player(f'Player {player+1}')
+        players.append(player)
     nicky = Player('Nicky')
-    #print(f'The first fixture is {home} vs {away}')
+    print('Enter 1 for a Home Win, 2 for a Draw or 3 for an Away Win')
+    print('---------------')
     for game in week_1:
+        num = game['game']
         home = game['teams'][0]
         away = game['teams'][1]
         result = game['result']
-        #question_1 = Question({home}, {away}, {result})
-        question_1 = Question(home, away, result)
-        question_1.fixture()
+        question = Question(home, away, result)
+        print(f'Game {num}')
+        question.fixture()
         nicky.prediction()
-        print(f'The correct answer is {question_1.correct}')
-        print(f'You guessed {nicky.answer}')
-        if nicky.answer == question_1.correct:
+        print(f'The correct answer is {ansConv(question.correct)}')
+        print(f'You guessed: {ansConv(nicky.answer)}')
+        if nicky.answer == question.correct:
             print('You guessed correct')
             nicky.score += nicky.answer
         else:
-            print('wrong answer')
+            print('Wrong answer')
+        print('---------------')
 
-        print(f'Your current score is {nicky.score}')
-
-
+    print(f'You scored {nicky.score}')
 
 
 main()
