687b07991b1e649832b0c72bb1f30c198eb19688
Al Smit <solanus@gmail.com>
2018-08-07 21:58:54 +0100

    app.py edit

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def rules():
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
