0fe51ef7870ecb1bb61b146ee15ab0bbb749c5e3
Al Smit <solanus@gmail.com>
2018-08-10 15:13:35 +0100

    env lines edit

--- app.py
+++ app.py
@@ -222,8 +222,6 @@ def rules():
 if __name__ == '__main__':
     # app.jinja_env.auto_reload = True
     # app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
     # app.run(debug=True)
     #app.run(debug=True, host='0.0.0.0')
