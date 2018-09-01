4b06271d897bd4854cc13381b496fd256474740c
Al Smit <solanus@gmail.com>
2018-09-01 12:32:32 +0100

    game route changed

--- app.py
+++ app.py
@@ -120,13 +120,14 @@ def enternames(id):
 @app.route("/game/<int:id>/<int:pNum>/<int:attempt>", methods=['GET', 'POST'])
 def game(id, pNum, attempt):
     form = AnswerForm()
+    print(players[0].name)
     if form.validate_on_submit():
         answer = form.answer.data
-        players[0].answer = answer
+        # players[0].answer = answer
         res = 1
         if answer == res:
             flash(f'You are correct {players[0].name}', 'success')
-            players[0].score = players[0].score +1
+            # players[0].score = players[0].score +1
             return redirect(url_for('game', id=id+1, pNum=pNum, attempt=1))
     return render_template('game.html', title='game', form=form, games=games,
                                    id=id, players=players, fixList=fixList, results=results, pNum=pNum)
