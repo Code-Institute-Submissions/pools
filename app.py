1664391fef4ccbf1fb363accbb79513a60c1a2f3
Al Smit <solanus@gmail.com>
2018-11-06 11:27:50 +0000

    functions moved, names changed

--- app.py
+++ app.py
@@ -1,104 +1,28 @@
-import os, json, random
-from flask import Flask, render_template, url_for, flash, redirect
+import os
+from dotenv import load_dotenv
+import json
+import random
+from flask import Flask, render_template, url_for, flash, redirect, session
 from forms import PlayerNumForm, NameForm, AnswerForm
-from game import Question, Player, calcWinner, createPlayerList, getPlayerName
+from game import Question, Player, calc_winner, create_player_list, get_player_name, add_to_highscores, get_highscores, get_rand_match_week, create_fixtures, get_correct_result, init_game, reset_highscores
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'nusmmirhdl4472'
+load_dotenv()
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
-# player objects
-# multiplayers = []
-# player scores dictionary
-highscores = []
-sortedArray = []
-topTen = []
+sorted_array = []
+top_ten = []
+fix_list = init_game()
+print(fix_list)
+multiplayers = create_player_list()
 
 
-def resetHighscores():
-    del highscores[:]
-
-
-def initFixtures(f):
-    f = []
-
-
-#read last line of scores.txt and add to highscores array
-def addToHighscores():
-    with open('scores.txt', 'r') as f:
-        player = {}
-        last_line = f.readlines()[-1]
-        name, score = last_line.split(':')
-        player['name'] = name
-        player['score'] = score
-        highscores.append(player)
-
-
-#function to extract scores from scores.txt and create dictionary of highscore player objects
-def getHighscores():
-    with open('scores.txt', 'r') as f:
-        for line in f:
-            player = {}
-            name, score = line.split(':')
-            player['name'] = name
-            player['score'] = int(score)
-            highscores.append(player)
-
-
-
-# get random matchweek object from json stripped file, contains 10 fixtures
-def getRandMatchWeek():
-    with open('2015_stripped.json') as f:
-        data = json.load(f)
-        # There are 38 fixture weeks in the season
-        randomWeek = random.randrange(38)
-        # Select random week from the json data
-        matchweek = data['rounds'][0]['matches']
-    return matchweek
-
-
-# create fixtures from extracted matchweek
-def createFixtures(obj):
-    gList = []
-    for game in obj:
-        home = game['team1']['name']
-        away = game['team2']['name']
-        result = game['result']
-        question = Question(home, away, result)
-        fixture = question.fixture()
-        gameDict = {'fixture': fixture,
-                    'result': result}
-        gList.append(gameDict)
-    return gList
-
-
-def getCorrectResult(num):
-    currentGame = num-1
-    result = fixList[currentGame]['result']
-    return result
-
-
-def initGame():
-    week = getRandMatchWeek()
-    fixtures = createFixtures(week)
-    list = fixtures
-    print('initgame ran')
-    return list
-
-
-fixList = initGame()
-print(fixList)
-multiplayers = createPlayerList()
-
-
-getHighscores()
-sortedHighscores = sorted(highscores, key=lambda item: int(item['score']), reverse=True)
-topTen = sortedHighscores[0:10]
-
-
-# multiplayers = createPlayerList()
+highscores = get_highscores()
+sorted_highscores = sorted(highscores, key=lambda item: int(item['score']), reverse=True)
+top_ten = sorted_highscores[0:10]
 
 
 @app.route("/")
@@ -107,21 +31,22 @@ def home():
     return render_template('home.html')
 
 
+
 @app.route("/newgame", methods=['GET', 'POST'])
 def newgame():
     with open('player_Names.txt', 'r+') as f:
         f.truncate(0)
     form = PlayerNumForm()
     if form.validate_on_submit():
-        numPlayers = int(form.players.data)
-        flash(f'{numPlayers} player game created!', 'dark')
-        return redirect(url_for('enternames', id=1, numPlayers=numPlayers))
+        num_players = int(form.players.data)
+        flash(f'{num_players} player game created!', 'dark')
+        return redirect(url_for('enternames', id=1, num_players=num_players))
     return render_template('newgame.html', title='newgame', form=form )
 
 
 
-@app.route("/enternames/<int:id>/<int:numPlayers>", methods=['GET', 'POST'])
-def enternames(id, numPlayers):
+@app.route("/enternames/<int:id>/<int:num_players>", methods=['GET', 'POST'])
+def enternames(id, num_players):
     form = NameForm()
     if form.validate_on_submit():
         name = form.playername.data
@@ -129,37 +54,34 @@ def enternames(id, numPlayers):
             name = name
             f.write(f'{name}\n')
         flash(f'Good luck {name}!! ', 'dark')
-        if numPlayers == 1:
+        if num_players == 1:
             return redirect(url_for('game', id=1, name=name, score=0, attempt=1))
-        elif id < numPlayers:
-            return redirect(url_for('enternames', id=id+1, numPlayers=numPlayers))
-        elif id == numPlayers:
-            # createPlayerList()
-            return redirect(url_for('multiplayer', id=1, pNum=1, attempt=1))
-    return render_template('enternames.html', form=form, id=id, numPlayers=numPlayers)
+        elif id < num_players:
+            return redirect(url_for('enternames', id=id+1, num_players=num_players))
+        elif id == num_players:
+            return redirect(url_for('multiplayer', id=1, p_num=1, attempt=1))
+    return render_template('enternames.html', form=form, id=id, num_players=num_players)
 
 
 
 @app.route("/game/<int:id>/<name>/<int:score>/<int:attempt>", methods=['GET', 'POST'])
 def game(id, name, score, attempt):
-    print(f'Length of multiplayers is {len(multiplayers)}')
-    print(f'Length of fixtures is {len(fixList)}')
     form = AnswerForm()
     if form.validate_on_submit():
         name = name
-        plrAnswer = form.answer.data
+        plr_answer = form.answer.data
         currId = id
-        correctRes = getCorrectResult(currId)
+        correct_result = get_correct_result(currId, fix_list)
         if id <= 9:
             if attempt == 1:
-                if plrAnswer != correctRes:
+                if plr_answer != correct_result:
                     flash(f'Wrong answer {name}, you have one more attempt', 'dark')
                     return redirect(url_for('game', id=id, name=name, score=score, attempt=2))
                 else:
                     flash(f'You are correct {name}', 'success')
-                    return redirect(url_for('game', id=id+1, name=name, score=score+plrAnswer, attempt=1))
+                    return redirect(url_for('game', id=id+1, name=name, score=score+plr_answer, attempt=1))
             elif attempt == 2:
-                if plrAnswer != correctRes:
+                if plr_answer != correct_result:
                     flash(f'Wrong answer {name}', 'dark')
                     return redirect(url_for('game', id=id+1, name=name, score=score, attempt=1))
                 else:
@@ -167,104 +89,89 @@ def game(id, name, score, attempt):
                     return redirect(url_for('game', id=id+1, name=name, score=score+1, attempt=1))
         elif id == 10:
             if attempt == 1:
-                if plrAnswer != correctRes:
+                if plr_answer != correct_result:
                     flash(f'Wrong answer {name}, you have one more attempt', 'dark')
                     return redirect(url_for('game', id=10, name=name, score=score, attempt=2))
                 else:
-                    score = score + plrAnswer
+                    score = score + plr_answer
                     return redirect(url_for('winner', name=name, score=score))
             if attempt == 2:
-                if plrAnswer != correctRes:
+                if plr_answer != correct_result:
                     return redirect(url_for('winner', name=name, score=score))
                 else:
                     score = score +1
                     return redirect(url_for('winner', name=name, score=score))
     return render_template('game.html', form=form,
-                                   id=id, fixList=fixList, name=name)
+                                   id=id, fix_list=fix_list, name=name)
 
 
 
 
-@app.route("/multiplayer/<int:id>/<int:pNum>/<int:attempt>", methods=['GET', 'POST'])
-def multiplayer(id, pNum, attempt):
-    print(f'Length of multiplayers is {len(multiplayers)}')
-    print(f'Length of fixtures is {len(fixList)}')
+@app.route("/multiplayer/<int:id>/<int:p_num>/<int:attempt>", methods=['GET', 'POST'])
+def multiplayer(id, p_num, attempt):
     form = AnswerForm()
     if form.validate_on_submit():
-        plrAnswer = form.answer.data
-        name = getPlayerName(multiplayers, pNum)
-        # count is the number of players
+        plr_answer = form.answer.data
+        name = get_player_name(multiplayers, p_num)
         count = len(multiplayers)
         currId = id
-        correctRes = getCorrectResult(currId)
+        correct_result = get_correct_result(currId, fix_list)
         fixtures = 10
-        # play game only for count number of players
-        if pNum <= count:
-            # last fixture, last player
-            if id == fixtures and pNum == count:
+        """ play game only for count number of players """
+        if p_num <= count:
+            """ last fixture, last player """
+            if id == fixtures and p_num == count:
                 if attempt == 1:
-                    if plrAnswer != correctRes:
+                    if plr_answer != correct_result:
                         flash(f'Wrong answer {name}, you have one more attempt', 'dark')
-                        return redirect(url_for('multiplayer', id=id, pNum=pNum, attempt=2))
+                        return redirect(url_for('multiplayer', id=id, p_num=p_num, attempt=2))
                     else:
-                        multiplayers[pNum-1].incScore(1, plrAnswer)
-                        print(multiplayers[pNum-1].name)
-                        print(multiplayers[pNum-1].score)
+                        multiplayers[p_num-1].inc_score(1, plr_answer)
                         return redirect(url_for('winnermult'))
                 elif attempt == 2:
-                    if plrAnswer != correctRes:
+                    if plr_answer != correct_result:
                         return redirect(url_for('winnermult'))
                     else:
-                        multiplayers[pNum-1].incScore(2, plrAnswer)
-                        print(multiplayers[pNum-1].name)
-                        print(multiplayers[pNum-1].score)
+                        multiplayers[p_num-1].inc_score(2, plr_answer)
                         return redirect(url_for('winnermult'))
             # if last player in a Question
-            elif pNum == count:
+            elif p_num == count:
                 if attempt == 1:
-                    if plrAnswer != correctRes:
+                    if plr_answer != correct_result:
                         flash(f'Wrong answer {name}, you have one more attempt', 'dark')
-                        return redirect(url_for('multiplayer', id=id, pNum=pNum, attempt=2))
+                        return redirect(url_for('multiplayer', id=id, p_num=p_num, attempt=2))
                     else:
                         flash(f'You are correct {name}', 'success')
-                        multiplayers[pNum-1].incScore(1, plrAnswer)
-                        print(multiplayers[pNum-1].name)
-                        print(multiplayers[pNum-1].score)
-                        return redirect(url_for('multiplayer', id=id+1, pNum=1, attempt=1))
+                        multiplayers[p_num-1].inc_score(1, plr_answer)
+                        return redirect(url_for('multiplayer', id=id+1, p_num=1, attempt=1))
                 elif attempt == 2:
-                    if plrAnswer != correctRes:
+                    if plr_answer != correct_result:
                         flash(f'Wrong answer {name}', 'dark')
-                        return redirect(url_for('multiplayer', id=id+1, pNum=1, attempt=1))
+                        return redirect(url_for('multiplayer', id=id+1, p_num=1, attempt=1))
                     else:
                         flash(f'You are correct {name}', 'success')
-                        multiplayers[pNum-1].incScore(2, plrAnswer)
-                        print(multiplayers[pNum-1].name)
-                        print(multiplayers[pNum-1].score)
-                        return redirect(url_for('multiplayer', id=id+1, pNum=1, attempt=1))
+                        multiplayers[p_num-1].inc_score(2, plr_answer)
+                        return redirect(url_for('multiplayer', id=id+1, p_num=1, attempt=1))
             # fixture 1 to 9 all players excluding last player
-            elif pNum < count:
+            elif p_num < count:
                 if attempt == 1:
-                    if plrAnswer != correctRes:
+                    if plr_answer != correct_result:
                         flash(f'Wrong answer {name}, you have one more attempt', 'dark')
-                        return redirect(url_for('multiplayer', id=id, pNum=pNum, attempt=2))
+                        return redirect(url_for('multiplayer', id=id, p_num=p_num, attempt=2))
                     else:
                         flash(f'You are correct {name}', 'success')
-                        multiplayers[pNum-1].incScore(1, plrAnswer)
-                        print(multiplayers[pNum-1].name)
-                        print(multiplayers[pNum-1].score)
-                        return redirect(url_for('multiplayer', id=id, pNum=pNum+1, attempt=1))
+                        multiplayers[p_num-1].inc_score(1, plr_answer)
+                        return redirect(url_for('multiplayer', id=id, p_num=p_num+1, attempt=1))
                 elif attempt == 2:
-                    if plrAnswer != correctRes:
+                    if plr_answer != correct_result:
                         flash(f'Wrong answer {name}', 'dark')
-                        return redirect(url_for('multiplayer', id=id, pNum=pNum+1, attempt=1))
+                        return redirect(url_for('multiplayer', id=id, p_num=p_num+1, attempt=1))
                     else:
                         flash(f'You are correct {name}', 'success')
-                        multiplayers[pNum-1].incScore(2, plrAnswer)
-                        print(multiplayers[pNum-1].name)
-                        print(multiplayers[pNum-1].score)
-                        return redirect(url_for('multiplayer', id=id, pNum=pNum+1, attempt=1))
+                        multiplayers[p_num-1].inc_score(2, plr_answer)
+                        return redirect(url_for('multiplayer', id=id, p_num=p_num+1, attempt=1))
     return render_template('multiplayer.html', form=form,
-                                   id=id, fixList=fixList, pNum=pNum, multiplayers=multiplayers)
+                                   id=id, fix_list=fix_list, p_num=p_num, multiplayers=multiplayers)
 
 
 
@@ -276,7 +183,7 @@ def winner(name, score):
         name = name
         score = score
         f.write(f'{name}:{score}\n')
-    return render_template('winner.html', calcWinner=calcWinner, highscores=highscores, name=name, score=score)
+    return render_template('winner.html', calc_winner=calc_winner, highscores=highscores, name=name, score=score)
 
 
 
@@ -284,23 +191,25 @@ def winner(name, score):
 def winnermult():
     multiplayers.sort(key=lambda x: x.score, reverse=True)
     if len(multiplayers) > 1:
-        calcWinner(multiplayers[0], multiplayers[1])
-    return render_template('winnermult.html', multiplayers=multiplayers, calcWinner=calcWinner, highscores=highscores)
+        calc_winner(multiplayers[0], multiplayers[1])
+    return render_template('winnermult.html', multiplayers=multiplayers, calc_winner=calc_winner, highscores=highscores)
 
 
 
 @app.route("/leaderboard", methods=['GET', 'POST'])
 def leaderboard():
-    resetHighscores()
-    getHighscores()
-    sortedHighscores = sorted(highscores, key=lambda item: item['score'], reverse=True)
-    topTen = sortedHighscores[0:10]
-    return render_template('leaderboard.html', title='leaderboard', highscores=highscores, sortedHighscores=sortedHighscores, topTen=topTen)
+    # reset_highscores(highscores)
+    get_highscores()
+    sorted_highscores = sorted(highscores, key=lambda item: item['score'], reverse=True)
+    top_ten = sorted_highscores[0:10]
+    return render_template('leaderboard.html', title='leaderboard', highscores=highscores, sorted_highscores=sorted_highscores, top_ten=top_ten)
+
 
 
 @app.route("/rules")
 def rules():
     return render_template('rules.html')
+
 
 
 if __name__ == '__main__':
--- game.py
+++ game.py
@@ -1,47 +1,63 @@
-# Question class used for creating a question instance containing 2 teams denoted as home and away
-# fixture method used for constructing the question
+import os
+import json
+import random
+
 class Question:
     def __init__(self, home, away, result):
+        """
+        Question class used for creating a question instance containing
+        2 teams denoted as home and away
+        """
         self.home = home
         self.away = away
         self.result = result
 
     def fixture(self):
+        """
+        fixture method used for constructing the question
+        """
         return(f'{self.home} vs {self.away}')
 
     def res(self):
         return(self.result)
 
 
-# Player class for creating instance of a player with a name and their scored
-# Player can make a prediction of the score of games
+
 class Player:
     def __init__(self, name='', score=0, answer=0):
+        """
+        Player class for creating instance of a player with a name and their scored
+        """
         self.name = name
         self.score = score
 
     def prediction(self):
+        """
+        Player can make a prediction of the score of games
+        """
         answer = input(f'Enter your prediction {self.name}.. ')
         self.answer = int(answer)
 
-    def incScore(self, attempt, guess):
+    def inc_score(self, attempt, guess):
         if attempt == 1:
             self.score = self.score + guess
         elif attempt == 2:
             self.score = self.score + 1
         return self.score
 
-    def getName(self):
+    def get_name(self):
         return self.name
 
-    def getScore(self):
+    def get_score(self):
         return self.score
 
 
 
-# Open file class for set up and taerdown of files
 class Open_File():
     def __init__(self, filename, mode):
+        """
+        Open file class for set up and taerdown of files
+        """
         self.filename = filename
         self.mode = mode
 
@@ -54,8 +70,10 @@ class Open_File():
 
 
 
-# Function to open txt file containing names, make list of player objects
-def createPlayerList():
+def create_player_list():
+    """
+    Function to open txt file containing names, make list of player objects
+    """
     with open ('player_Names.txt', 'r') as f:
         list = []
         for name in f:
@@ -64,109 +82,96 @@ def createPlayerList():
     return list
 
 
-# Get Player Name
-def getPlayerName(list, num):
+
+def get_player_name(list, num):
     name = list[num -1].name
     return name
 
 
-# Get Player Name List
-def getPlayerNameList():
-    names = []
-    with open('player_Names.txt', 'r') as f:
-        for line in f:
-            name = line
-            names.append(name)
-        pName = names
-        return pName
 
-
-# Function to convert guess input to string
-def ansConv(guess):
-    if guess == 1:
-        ansStr = 'Home Win'
-    elif guess == 2:
-        ansStr = 'Draw'
-    else:
-        ansStr = 'Away Win'
-    return ansStr
-
-
-def checkAnswer(plrAnswer, correctRes):
-    if plrAnswer != correctRes:
-        flash(f'Wrong answer {name}, you have one more attempt', 'dark')
-        return redirect(url_for('game', id=id, name=name, score=score, attempt=2))
-    if plrAnswer == correctRes:
-        # flash(f'You are correct {name}', 'success')
-        return redirect(url_for('game', id=id+1, name=name, score=score+1, attempt=1))
-
-
-
-# Function to calculate if there is a winner.  It only needs the top 2 sorted player objects.
-# If these 2 scores are equal then its a draw and any remaining players dont matter
-def calcWinner(playerA, playerB):
+def calc_winner(playerA, playerB):
+    """
+    Function to calculate if there is a winner.  It only needs the top 2 sorted player objects.
+    If these 2 scores are equal then its a draw and any remaining players dont matter
+    """
     if playerA.score > playerB.score:
-        #change return to print to function in console
         return(f'{playerA.name} is the winner!')
     else:
         return('Game has finished in a draw!')
 
 
 
-def main():
-    # initialize empty list to store player objects
-    players = []
-    max_players = 10
-    # Prompt for number of players
-    num_players = input('Please enter number of players..')
-    # Prompt for number of weeks/length of game
-    num_weeks = int(input('Please enter number of weeks you would like to play..'))
-    #Create player objects
-    for player in range(int(num_players)):
-        player = Player(f'Player {player+1}')
-        players.append(player)
-
-    print('Enter 1 for a Home Win, 2 for a Draw or 3 for an Away Win')
-    print('---------------')
-    # wk_num used to calculate week number on gui
-    wk_num = 1
-    # count used to keep track of current week
-    count = 0
-    # Traverse season Data Structure to evaluate player input
-    for week in season:
-        # while loop used to play as many weeks selected by user
-        while count < num_weeks:
-            for game in week:
-                num = game['game']
-                home = game['teams'][0]
-                away = game['teams'][1]
-                result = game['result']
-                question = Question(home, away, result)
-                print(f'Week {wk_num} Game {num}')
-                question.fixture()
-                for player in players:
-                    player.prediction()
-                    #print(f'The correct prediction is {ansConv(question.correct)}')
-                    print(f'You guessed: {ansConv(player.answer)}')
-                    if player.answer == question.correct:
-                        print('Correct prediction!')
-                        player.score += player.answer
-                    else:
-                        print('Incorrect prediction!')
-                    print('---------------')
-            wk_num += 1
-            count += 1
-
-    # Sort players by score
-    players.sort(key=lambda x: x.score, reverse=True)
-
-    # Print player leaderboard
-    for player in players:
-        print(f'{player.name} scored {player.score} points.')
-        print('---------------')
-
-    # Use calcWinner function on sorted player list to establish draw or winner
-    calcWinner(players[0], players[1])
+def add_to_highscores():
+    """ read last line of scores.txt and add to highscores array """
+    with open('scores.txt', 'r') as f:
+        player = {}
+        last_line = f.readlines()[-1]
+        name, score = last_line.split(':')
+        player['name'] = name
+        player['score'] = score
+        highscores.append(player)
 
 
-#main()
+
+def get_highscores():
+    """
+    function to extract scores from scores.txt and
+    create dictionary of highscore player objects
+    """
+    highscores = []
+    with open('scores.txt', 'r') as f:
+        for line in f:
+            player = {}
+            name, score = line.split(':')
+            player['name'] = name
+            player['score'] = int(score)
+            highscores.append(player)
+    return highscores
+
+
+
+def get_rand_match_week():
+    """
+    get random matchweek object from json stripped file, contains 10 fixtures
+    """
+    with open('38_week_season_results.json') as f:
+        data = json.load(f)
+        """ Season is 38 weeks long """
+        randomWeek = random.randrange(38)
+        """ Select random week from the json """
+        matchweek = data['rounds'][0]['matches']
+    return matchweek
+
+
+
+def create_fixtures(obj):
+    """ create fixtures from extracted matchweek """
+    gList = []
+    for game in obj:
+        home = game['team1']['name']
+        away = game['team2']['name']
+        result = game['result']
+        question = Question(home, away, result)
+        fixture = question.fixture()
+        gameDict = {'fixture': fixture,
+                    'result': result}
+        gList.append(gameDict)
+    return gList
+
+
+
+def get_correct_result(num, list):
+    currentGame = num-1
+    result = list[currentGame]['result']
+    return result
+
+
+def init_game():
+    week = get_rand_match_week()
+    fixtures = create_fixtures(week)
+    list = fixtures
+    return list
+
+
+def reset_highscores(list):
+    del list[:]
