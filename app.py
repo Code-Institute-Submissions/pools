40c63b1012d5c6f81fb793db8fa71765e1ada4e4
Al Smit <solanus@gmail.com>
2019-01-06 00:33:42 +0000

    2 player test

--- app.py
+++ app.py
@@ -52,17 +52,15 @@ def enternames(id, num_players):
     if form.validate_on_submit():
         name = form.playername.data
         names.append(name)
-        multiplayers[int(id)-1].name = name
+        # multiplayers[int(id)-1].name = name
         flash(f'Good luck {name}!! ', 'dark')
         if num_players == 1:
             return redirect(url_for('game', id=1, name=name, score=0, attempt=1))
         elif id < num_players:
-            # names.append(name)
-            # multiplayers[int(id)-1].name = name
             return redirect(url_for('enternames', id=id+1, num_players=num_players))
         elif id == num_players:
             names.append(name)
-            multiplayers[int(id)-1].name = name
+            # multiplayers[int(id)-1].name = name
             return redirect(url_for('multiplayer', id=1, p_num=1, attempt=1))
     return render_template('enternames.html', form=form, id=id, num_players=num_players)
 
