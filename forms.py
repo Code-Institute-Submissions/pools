91ab25df93786a81d19764e9a9fb223003537a56
Al Smit <solanus@gmail.com>
2019-01-11 18:27:07 +0000

    2 player mode functioning

--- forms.py
+++ forms.py
@@ -4,12 +4,12 @@ from wtforms.validators import DataRequired, Length, NumberRange
 
 
 class PlayerNumForm(FlaskForm):
-    players = IntegerField('Please enter number of players, maximum is five',
-                           validators=[DataRequired(), NumberRange(min=1, max=5)])
+    players = IntegerField('Enter 1 for 1 Player mode or 2 for 2 Player mode',
+                           validators=[DataRequired(), NumberRange(min=1, max=2)])
     submit = SubmitField('Confirm')
 
 class NameForm(FlaskForm):
-    playername = StringField('Name', validators=[DataRequired(), Length(min=2, max=10)])
+    playername = StringField('Name', validators=[DataRequired(), Length(min=2, max=16)])
     submit = SubmitField('Enter Name')
 
 class AnswerForm(FlaskForm):
